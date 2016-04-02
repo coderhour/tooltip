218ceb0abfd6a533ebad87b10739cff569b29096
coderhour <coderhour@gmail.com>
2016-04-01 22:36:36 -0700

    Init.

---
+++ app/src/main/java/com/coderhour/tooltippopupwindow/MainActivity.java
@@ -0,0 +1,83 @@
+package com.coderhour.tooltippopupwindow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.coderhour.tooltip.TooltipPopupWindow;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mBtnLeft, mBtnTop, mBtnRight, mBtnBottom;
+
+    private TextView mContentView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnLeft = (Button) findViewById(R.id.btnLeft);
+        mBtnTop = (Button) findViewById(R.id.btnTop);
+        mBtnRight = (Button) findViewById(R.id.btnRight);
+        mBtnBottom = (Button) findViewById(R.id.btnBottom);
+
+        mBtnLeft.setOnClickListener(this);
+        mBtnTop.setOnClickListener(this);
+        mBtnRight.setOnClickListener(this);
+        mBtnBottom.setOnClickListener(this);
+
+        mContentView = new TextView(this);
+        mContentView.setText("Happy Friday, everyone! What better way to enjoy the start of your weekend" +
+                "\n than by getting a bunch of paid iPhone apps that are on sale for free right now for a " +
+                "\nlimited time? Today we've rounded up 12, yes 12, iPhone and iPad apps that would normally" +
+                "\n cost $41 but are on sale for free if you act right now. Let's check them out!" +
+                "\n" +
+                "\nMUST SEE: Videos offer first look at what it’s like driving Tesla’s Model 3" +
+                "\n" +
+                "\nThese are paid iPhone and iPad apps that have been made available for free for a limited" +
+                "\n time by their developers. There is no way to tell how long they will be free. These sales " +
+                "\ncould end an hour from now or a week from now — obviously, the only thing we can guarantee " +
+                "\nis that they were free at the time this post was written. If you click on a link and see a" +
+                "\n price listed next to an app instead of the word “get,” it is no longer free. The sale has ended." +
+                "\n If you download the app, you will be charged.");
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnLeft) {
+            new TooltipPopupWindow(mContentView, WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT)
+                    .setWindowPosition(TooltipPopupWindow.POS_LEFT)
+                    .setAnchorView(mBtnLeft)
+                    .setOutsideTouchableChain(true)
+                    .show(getWindow().getDecorView());
+        } else if (view.getId() == R.id.btnTop) {
+            new TooltipPopupWindow(mContentView, WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT)
+                    .setWindowPosition(TooltipPopupWindow.POS_TOP)
+                    .setAnchorView(mBtnTop)
+                    .setOutsideTouchableChain(true)
+                    .show(getWindow().getDecorView());
+        } else if (view.getId() == R.id.btnRight) {
+            new TooltipPopupWindow(mContentView, WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT)
+                    .setWindowPosition(TooltipPopupWindow.POS_RIGHT)
+                    .setAnchorView(mBtnRight)
+                    .setOutsideTouchableChain(true)
+                    .show(getWindow().getDecorView());
+        } else if (view.getId() == R.id.btnBottom) {
+            new TooltipPopupWindow(mContentView, WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT)
+                    .setWindowPosition(TooltipPopupWindow.POS_BOTTOM)
+                    .setAnchorView(mBtnBottom)
+                    .setOutsideTouchableChain(true)
+                    .show(getWindow().getDecorView());
+        }
+
+    }
+
+}
---
+++ tooltip/src/main/java/com/coderhour/tooltip/TooltipPopupWindow.java
@@ -0,0 +1,168 @@
+package com.coderhour.tooltip;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
+/**
+ * @author blade
+ * @version 1.0: 4/1/16
+ */
+public class TooltipPopupWindow extends PopupWindow {
+
+    public static final int POS_LEFT = 0;
+    public static final int POS_RIGHT = 1;
+    public static final int POS_TOP = 2;
+    public static final int POS_BOTTOM = 3;
+
+    private LinearLayout mLinearLayout;
+    private ImageView mImageView;
+    private int mX, mY;
+    private int mPos = -1;
+    private View mOriginalView;
+
+    public TooltipPopupWindow(Context context) {
+        super(context);
+    }
+
+    public TooltipPopupWindow(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TooltipPopupWindow(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TooltipPopupWindow(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public TooltipPopupWindow() {
+    }
+
+    public TooltipPopupWindow(View contentView) {
+        super(contentView);
+    }
+
+    public TooltipPopupWindow(int width, int height) {
+        super(width, height);
+    }
+
+    public TooltipPopupWindow(View contentView, int width, int height) {
+        super(contentView, width, height);
+    }
+
+    public TooltipPopupWindow(View contentView, int width, int height, boolean focusable) {
+        super(contentView, width, height, focusable);
+    }
+
+    public TooltipPopupWindow setBackgroundColor(int color) {
+        return this;
+    }
+
+    public TooltipPopupWindow setBackgroundColorResource(int colorRes) {
+        return this;
+    }
+
+    public TooltipPopupWindow setWindowPosition(int pos) {
+        mPos = pos;
+        mOriginalView = getContentView();
+        mLinearLayout = new LinearLayout(mOriginalView.getContext());
+        mImageView = new ImageView(mOriginalView.getContext());
+        mOriginalView.setBackgroundResource(R.drawable.round_corner_background);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams ivParams = new LinearLayout.LayoutParams(
+                64, 64);
+        if (mOriginalView.getParent() != null) {
+            ((LinearLayout)mOriginalView.getParent()).removeAllViews();
+        }
+        if (pos == POS_LEFT) {
+            mLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            mLinearLayout.addView(mOriginalView);
+
+            mImageView.setBackgroundResource(R.drawable.triangle_left);
+            mLinearLayout.addView(mImageView, ivParams);
+        } else if (pos == POS_TOP) {
+            // W/A Start
+            mLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            mLinearLayout.addView(mOriginalView);
+
+            mImageView.setBackgroundResource(R.drawable.triangle_left);
+            mLinearLayout.addView(mImageView, ivParams);
+
+            mLinearLayout.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+            mLinearLayout.removeAllViews();
+            // W/A End
+
+            mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+            mLinearLayout.addView(mOriginalView);
+
+            mImageView.setBackgroundResource(R.drawable.triangle_top);
+            mLinearLayout.addView(mImageView, ivParams);
+        } else if (pos == POS_RIGHT) {
+            mLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+            mImageView.setBackgroundResource(R.drawable.triangle_right);
+            mLinearLayout.addView(mImageView, ivParams);
+
+            mLinearLayout.addView(mOriginalView, params);
+        } else if (pos == POS_BOTTOM) {
+            mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+
+            mImageView.setBackgroundResource(R.drawable.triangle_bottom);
+            mLinearLayout.addView(mImageView, ivParams);
+
+            mLinearLayout.addView(mOriginalView, params);
+        }
+        setContentView(mLinearLayout);
+        return this;
+    }
+
+    public TooltipPopupWindow setAnchorView(View anchorView) {
+        mLinearLayout.setPadding(0, 0, 0, 0);
+        mLinearLayout.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        if (mPos == -1) {
+            throw new IllegalStateException("Pleas call setWindowPosition first.");
+        }
+        int[] loc = new int[2];
+        anchorView.getLocationOnScreen(loc);
+        if (mPos == POS_LEFT) {
+            mX = loc[0] - mLinearLayout.getMeasuredWidth() + anchorView.getPaddingLeft();
+            mY = loc[1] + anchorView.getPaddingTop() - mLinearLayout.getMeasuredHeight() / 2 + anchorView.getHeight() / 2;
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) mImageView.getLayoutParams();
+            params.topMargin = loc[1] - mY + anchorView.getHeight() / 2 - mImageView.getMeasuredHeight() / 2;
+        } else if (mPos == POS_TOP) {
+            mX = loc[0] + anchorView.getPaddingLeft() - mLinearLayout.getMeasuredWidth() / 2 + anchorView.getWidth() / 2;
+            mY = loc[1] - mLinearLayout.getMeasuredHeight() - anchorView.getPaddingTop();
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) mImageView.getLayoutParams();
+            params.leftMargin = loc[0] - mX + anchorView.getWidth() / 2 - mImageView.getMeasuredWidth() / 2;
+        } else if (mPos == POS_RIGHT) {
+            mX = loc[0] + anchorView.getWidth() - anchorView.getPaddingRight();
+            mY = loc[1] + anchorView.getPaddingTop() - mLinearLayout.getMeasuredHeight() / 2 + anchorView.getHeight() / 2;
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) mImageView.getLayoutParams();
+            params.topMargin = loc[1] - mY + anchorView.getHeight() / 2 - mImageView.getMeasuredHeight() / 2;
+        } else if (mPos == POS_BOTTOM) {
+            mX = loc[0] + anchorView.getPaddingLeft() - mLinearLayout.getMeasuredWidth() / 2 + anchorView.getWidth() / 2;
+            mY = loc[1] + anchorView.getHeight() - anchorView.getPaddingBottom();
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) mImageView.getLayoutParams();
+            params.leftMargin = loc[0] - mX + anchorView.getWidth() / 2 - mImageView.getMeasuredWidth() / 2;
+        }
+        return this;
+    }
+
+    public TooltipPopupWindow setOutsideTouchableChain(boolean touchable) {
+        super.setOutsideTouchable(touchable);
+        return this;
+    }
+
+    public void show(View parent) {
+        super.showAtLocation(parent, Gravity.TOP | Gravity.LEFT, mX, mY);
+    }
+
+}
